f03445e05b9b3e91feeabb59fc01f422462b631c
prabn kumar sahu <prabinkumarshu28@gmail.com>
2021-03-21 16:44:33 +0530

    gradle update

---
+++ app/src/main/java/com/example/shareanalysiscalculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.shareanalysiscalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
